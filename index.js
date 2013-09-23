3caefbdccdc56cedc09977458dcde675b64fa5f9
Michal Kuklis <michal.kuklis@gmail.com>
2013-09-22 23:40:17 -0400

    updated readme

--- index.js
+++ index.js
@@ -2,24 +2,24 @@ var Emitter = require('emitter');
 var SimpleSocket = require('simplesocket');
 
 var client = module.exports = {
-	connect: function (url, options) {
-		var self = this;
-		this.socket = new SimpleSocket(url, null, options);
-		
-		this.socket.onmessage = function (event) {
-			try {
-				var data = JSON.parse(event.data);
-				if (data.name) {
-					self.emit(data.name, data.args);
-				}
-			}
-			catch (error) {
+  connect: function (url, options) {
+    var self = this;
+    this.socket = new SimpleSocket(url, null, options);
+    
+    this.socket.onmessage = function (event) {
+      try {
+        var data = JSON.parse(event.data);
+        if (data.name) {
+          self.emit(data.name, data.args);
+        }
+      }
+      catch (error) {
 
-			}
-		}
+      }
+    }
 
-		return this;
-	}
+    return this;
+  }
 };
 
 Emitter(client);
